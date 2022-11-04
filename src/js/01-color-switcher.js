863c3150b223b61f5e9a0d185c7c0b522ebd0e63
Elena Pastushenko <heleora_net@ukr.net>
2022-11-04 10:52:45 +0200

    Task 2 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+// const refs = {
+//     startBtn: document.querySelector("button[data-start]"),
+//     stopBtn: document.querySelector("button[data-stop]"),
+// }
 
+// refs.startBtn.style.backgroundColor = "green";
+// refs.stopBtn.style.backgroundColor = "red";
+
+const refs = {
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+    body: document.querySelector("body"),
+    };
+    let timerID = 0;
+    
+    refs.startBtn.addEventListener("click", changeBodyColor);
+    refs.stopBtn.addEventListener("click", stopBodyColor);
+    
+    function changeBodyColor (evt) {
+    refs.stopBtn.removeAttribute("disabled");
+    evt.target.setAttribute("disabled", true);
+    timerID = setInterval( () => {
+    refs.body.style.backgroundColor = getRandomHexColor()
+    }, 
+    1000);
+    };
+    
+    function stopBodyColor (evt) {
+    clearInterval(timerID);
+    evt.target.setAttribute("disabled", true);
+    refs.startBtn.removeAttribute("disabled");
+    };
+    
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    };
