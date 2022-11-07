29712071a0f72a1dad52708b0af450ea6a361cfa
Elena Pastushenko <heleora_net@ukr.net>
2022-11-07 23:34:48 +0200

    Task 3 is done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,69 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+import Notiflix from 'notiflix';
+
+
+  const form = document.querySelector(".form");
+  const createPromiseBtn = document.querySelector("button");
+
+
+form.addEventListener("submit", onFormSubmit);
+
+ 
+
+function onFormSubmit (evt) {
+  evt.preventDefault();
+  
+  const formElements = evt.currentTarget.elements;
+  const delay = formElements.delay.value;
+  const step = formElements.step.value;
+  const amount = formElements.amount.value;
+  
+  
+  let position = 0;
+  
+  for (let i = 0; i < amount; i++) {
+    createPromise(position, delay, step)
+    .then(({ position, resultDelay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${resultDelay}ms`);
+    })
+    .catch(({ position, resultDelay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${resultDelay}ms`);});
+      position += 1;
+    }
+    
+    evt.currentTarget.reset;
   }
-}
+
+function createPromise(position, delay, step) {
+  
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const resultDelay = Number(delay) + Number(position) * Number(step);
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+     resolve({ position, resultDelay });
+    }
+    else {
+      // Reject
+      reject ({ position, resultDelay });
+    }
+  }
+  , resultDelay
+    )
+
+  })
+};
+
+// createPromise().then(value => 
+//   Notiflix.Report.success("Success!", value))
+//   .catch(error => Notiflix.Report.failure("Error!", error));
+
+
+// for (let i = 0; i < amount; i++) {
+//   createPromise(position, delay, step)
+//   .then(value => console.log(value)) 
+//   .catch(error => console.log(error));
+//   position += 1;
+// }
